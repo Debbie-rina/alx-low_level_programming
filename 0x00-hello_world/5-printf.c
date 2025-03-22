a972b49c4f80a556c70e10b5104290fc20e6a9db
Debbie-rina <rinadebby5@gmail.com>
2025-03-22 12:20:10 -0700

    learning betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
-/*
- * main-Entry point
+/**
+ * main - Entry point
  *
- * function-outputs a string using printf
+ *description:outputs a string using printf
+ *
+ *return: always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
 
