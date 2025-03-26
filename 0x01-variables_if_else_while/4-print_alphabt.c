abe55c1bef3d84c4ea15a080abe0497cc9e1687f
Debbie-rina <rinadebby5@gmail.com>
2025-03-25 21:18:01 -0700

    spaces are important

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,16 @@
 /**
  * main -Entry point
  *
- * Description:prints alphabet apart from 
+ * Description:print alphabet apart from
  * some specified not to be printed
  *
  * Return:Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 {
 	if (letter == 'q' || letter == 'e')
 		continue;
