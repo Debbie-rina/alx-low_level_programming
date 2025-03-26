5bc67e9e3725e5d66852ca912fa712638e0ead3a
Debbie-rina <rinadebby5@gmail.com>
2025-03-25 20:26:30 -0700

    alphabets and putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:program outputs the alphabet in lowercase
+ * from a-z
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a';letter <= 'z';letter++)
+{
+	putchar(letter);
+}
+	putchar('\n');
+	
+	return (0);
+}
