bfd1339c1e4b5179ecbabb6b222e6fed10cb2b6d
Debbie-rina <rinadebby5@gmail.com>
2025-03-27 09:46:02 -0700

    no new line

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description:Prints all possible combinations of single-digit numbers
+ * Numbers are separated by ", " and printed in ascending order
+ * Ends with "$" instead of a newline
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int des; 
+
+	for (des = 0; des <= 9; des++)
+	{
+		putchar(des + '0');
+
+		if (des != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		
+	}
+	putchar('$');
+	return (0);
+
+}
