c3025777c6931b134f0255be4d9415c4e3011d4e
Debbie-rina <rinadebby5@gmail.com>
2025-05-07 08:33:51 -0700

    no braces in comments

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,6 @@
  * @c: The character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
- *
- * alternate code
- * int _islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
  */
 int _islower(int c)
 {
